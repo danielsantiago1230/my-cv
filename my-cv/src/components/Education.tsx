89dfe2218cb5e5559aa0d1010447606e5a3ac586
Santiago <danielsantiago1230@gmail.com>
2024-11-26 22:34:39 -0600

    Updated education

--- my-cv/src/components/Education.tsx
+++ my-cv/src/components/Education.tsx
@@ -8,79 +8,33 @@ const Education: React.FC = () => {
 
             <div className="space-y-6">
                 <Card
-                    title="Meta Back-End Developer"
-                    subtitle="Coursera"
+                    title="Meta Back-End Developer - Coursera"
+                    subtitle="Completed a comprehensive program focusing on back-end development, including:"
                     date="Feb 2023 - Dec 2023"
+                    links={[
+                        { label: 'View Certificate', url: 'https://www.coursera.org/account/accomplishments/specialization/X5MRR8WSJBXQ' },
+                    ]}
                     description={[
-                        'Skills: MySQL, Django REST Framework, Python, Back-End Web Development',
+                        'API Development and REST architecture using Django.',
+                        'Building scalable systems with Django REST Framework.',
+                        'Database Management with MySQL and SQL.',
+                        'Version Control using Git and GitHub.',
+                        'Programming in Python, with emphasis on data structures, OOP, and error handling.',
+                        'Front-end basics with HTML5, Bootstrap, and CSS3 for back-end integration.',
                     ]}
                 />
 
                 <Card
-                    title="Certificate in APIs by Meta"
-                    subtitle="Coursera"
-                    date="Nov 2023 - Dec 2023"
-                    description={[
-                        'Focus: API Development, REST architecture, Django',
-                    ]}
-                />
-
-                <Card
-                    title="Certificate in Django Web Framework Development"
-                    subtitle="Coursera"
-                    date="Aug 2023 - Oct 2023"
-                    description={[
-                        'Skills: Django REST Framework, Python, Django',
-                    ]}
-                />
-
-                <Card
-                    title="Certificate in Databases for Back-End Development"
-                    subtitle="Coursera"
-                    date="May 2023 - Jul 2023"
-                    description={[
-                        'Skills: MySQL, SQL, Database Management',
-                    ]}
-                />
-
-                <Card
-                    title="Certificate in Version Control"
-                    subtitle="Coursera"
-                    date="Apr 2023 - May 2023"
-                    description={[
-                        'Skills: Git, GitHub',
-                    ]}
-                />
-
-                <Card
-                    title="Certificate in Programming in Python"
-                    subtitle="Coursera"
-                    date="Mar 2023 - Apr 2023"
-                    description={[
-                        'Skills: Python, Data Structures, OOP, Error Handling',
-                    ]}
-                />
-
-                <Card
-                    title="Certificate in Back-End Development"
-                    subtitle="Coursera"
-                    date="Feb 2023 - Mar 2023"
-                    description={[
-                        'Skills: HTML5, Bootstrap, CSS3',
-                    ]}
-                />
-
-                <Card
-                    title="React.js Course"
+                    title="Certificate in React.js"
                     subtitle="Centro de e-Learning UTN FRBA"
                     date="Feb 2022 - May 2022"
                     description={[
-                        'Skills: React JS, Firebase, JavaScript ES6',
+                        'Skills: React.js, Firebase, JavaScript ES6',
                     ]}
                 />
 
                 <Card
-                    title="Professional Front End Developer"
+                    title="Professional Front-End Developer Certification"
                     subtitle="Centro de e-Learning UTN FRBA"
                     date="Oct 2021 - Apr 2022"
                     description={[
@@ -89,24 +43,30 @@ const Education: React.FC = () => {
                 />
 
                 <Card
-                    title="Advanced Web Developer (HTML5, JS6, CSS3)"
+                    title="Advanced Web Developer Certification"
                     subtitle="Centro de e-Learning UTN FRBA"
                     date="Feb 2022 - Apr 2022"
-                    description={[]}
+                    description={[
+                        'Skills: HTML5, CSS3, JavaScript, JQuery, Bootstrap',
+                    ]}
                 />
 
                 <Card
-                    title="Javascript Web Development"
+                    title="JavaScript Web Development Certification"
                     subtitle="Centro de e-Learning UTN FRBA"
                     date="Dec 2021 - Feb 2022"
-                    description={[]}
+                    description={[
+                        'Skills: JavaScript, JQuery',
+                    ]}
                 />
 
                 <Card
                     title="Bachelor of Electronic Engineering"
                     subtitle="Universidad Nacional de La Plata"
                     date="Jan 2014 - Dec 2021 (65% Completed)"
-                    description={[]}
+                    description={[
+                        'Skills: C, Software Abstraction, Embedded Systems, Innovative Thinking, Problem-Solving',
+                    ]}
                 />
             </div>
         </section>
